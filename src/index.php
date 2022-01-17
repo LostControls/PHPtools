1180088ef85b0e2052b42a8b69f91229cb5350e1
LostControls <1447292956@qq.com>
2022-01-17 17:36:29 +0800

    test error

--- src/index.php
+++ src/index.php
@@ -7,11 +7,17 @@ namespace Lostcontrols\PHPtools;
 
 require_once '../vendor/autoload.php';
 
-use Lostcontrols\PHPtools\Hash\BcryptHash;
+//use Lostcontrols\PHPtools\Hash\BcryptHash;
+use Lostcontrols\PHPtools\Log\Log;
 
+///** @noinspection PhpUnhandledExceptionInspection */
+//$b = callToolsMethod(BcryptHash::class, 'generate', '557184');
+//$a = callToolsMethod(BcryptHash::class, 'check', '1447292956,$2y$10$gBqKJNngmmhixGNFIZltKejuXEQMXWL9l.ZcZ./gDcX0thvZXMmE6');
+$data = [
+    'name' => '张三',
+    'sex' => 1,
+    'age' => 30,
+    'address' => '长沙'
+];
 /** @noinspection PhpUnhandledExceptionInspection */
-$b = callToolsMethod(BcryptHash::class, 'generate', '557184');
-/** @noinspection PhpUnhandledExceptionInspection */
-$a = callToolsMethod(BcryptHash::class, 'check', '1447292956,$2y$10$gBqKJNngmmhixGNFIZltKejuXEQMXWL9l.ZcZ./gDcX0thvZXMmE6');
-var_dump($b);
-dd($a);
+callToolsMethod(Log::class,'debug',['debug',$data]);
